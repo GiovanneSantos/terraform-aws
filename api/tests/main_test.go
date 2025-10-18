dc900c7dce8b3b164f69a60686374a3588ef9d2f
Giovanne <giovanne.vieira@outlook.com>
2025-10-18 11:24:33 -0300

    Muda o main taste

--- api/tests/main_test.go
+++ api/tests/main_test.go
@@ -1,15 +1,17 @@
-package main
+package main_test  // diferente de main
 
 import (
 	"net/http/httptest"
 	"testing"
+
+	"github.com/GiovanneSantos/terraform-aws/api" // importa o pacote da API
 )
 
 func TestHelloHandler(t *testing.T) {
 	req := httptest.NewRequest("GET", "/", nil)
 	w := httptest.NewRecorder()
 
-	HelloHandler(w, req)
+	api.HelloHandler(w, req)
 
 	if w.Body.String() != "Hello, GO API!" {
 		t.Errorf("unexpected body: got %v", w.Body.String())

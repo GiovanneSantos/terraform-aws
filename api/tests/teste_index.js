6b5a6e90952c9fe3665390250193dfbbf65f068a
Giovanne <giovanne.vieira@outlook.com>
2025-10-18 12:31:47 -0300

    c

--- api/tests/teste_index.js
+++ api/tests/teste_index.js
@@ -11,7 +11,7 @@ const req = http.request(options, res => {
   let data = '';
   res.on('data', chunk => data += chunk);
   res.on('end', () => {
-    if (data === 'Hello Node API!') {
+    if (data === 'API running') {  // agora bate com a resposta da sua API
       console.log('Test passed');
     } else {
       console.error('Test failed:', data);

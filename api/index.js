1f54d8ea5b38993d06780c0f85a1e271748489cc
Giovanne <giovanne.vieira@outlook.com>
2025-10-18 11:55:32 -0300

    Corrigindo nome

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  res.send('Hello, Node API!');
+  res.send('Hello, Nodes API!');
 });
 
 app.listen(PORT, () => {

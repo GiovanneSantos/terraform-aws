e1d2df1454afb8d5a2f3c081f8d8b25871882f62
Giovanne <giovanne.vieira@outlook.com>
2025-10-18 11:10:45 -0300

    Corrigindo o teste

--- api/tests/main_test.go
+++ api/tests/main_test.go
@@ -1,18 +1,15 @@
-package main
-
 import (
-	"net/http"
-	"net/http/httptest"
-	"testing"
+    "testing"
+    "github.com/GiovanneSantos/terraform-aws/api"
 )
 
 func TestHelloHandler(t *testing.T) {
-	req := httptest.NewRequest("GET", "/", nil)
-	w := httptest.NewRecorder()
+    req := httptest.NewRequest("GET", "/", nil)
+    w := httptest.NewRecorder()
 
-	HelloHandler(w, req)
+    api.HelloHandler(w, req)
 
-	if w.Body.String() != "Hello, GO API!" {
-		t.Errorf("expected 'Hello, GO API!', got '%s'", w.Body.String())
-	}
+    if w.Body.String() != "Hello, GO API!" {
+        t.Errorf("unexpected body: got %v", w.Body.String())
+    }
 }

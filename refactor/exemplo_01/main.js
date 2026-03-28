170df7e995284e9c86f0ca3badd029b649a8297c
Giovanne <giovanne.vieira@outlook.com>
2026-03-28 12:53:51 -0300

    corrigindo valores

--- refactor/exemplo_01/main.js
+++ refactor/exemplo_01/main.js
@@ -1,30 +1,33 @@
 import invoices from "./invoices.json" with {type: 'json'} ;
 import plays from "./plays.json" with {type: 'json'};
 
-function Statement (invoices, plays){
+function statement(invoice, plays) {
     let totalAmount = 0;
     let volumeCredits = 0;
-    let result = `Statment for ${invoices.customer}\n`;
+    let result = `Statement for ${invoice.customer}\n`;
 
-    const format = new Intl.NumberFormat("en-US",
-        { style: "currency", currency: "USD",
-        minimumFractionDigits: 2 }).format;
+    const format = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+        minimumFractionDigits: 2
+    }).format;
 
-    for (let perf of invoices.performances) {
-        let thisAmount = 0;
+    for (let perf of invoice.performances) {
         const play = plays[perf.playId];
+        let thisAmount = 0;
+
         switch (play.type) {
             case "tragedy":
-                thisAmount = 4000;
-                if(perf.audience > 30) {
+                thisAmount = 40000;
+                if (perf.audience > 30) {
                     thisAmount += 1000 * (perf.audience - 30);
                 }
                 break;
 
             case "comedy":
-                thisAmount = 3000;
+                thisAmount = 30000;
                 if (perf.audience > 20) {
-                    thisAmount += 1000 + 500 * (perf.audience - 20);
+                    thisAmount += 10000 + 500 * (perf.audience - 20);
                 }
                 thisAmount += 300 * perf.audience;
                 break;
@@ -33,27 +36,26 @@ function Statement (invoices, plays){
                 throw new Error(`unknown type: ${play.type}`);
         }
 
-        // soma créditos por volume
+        // add volume credits
         volumeCredits += Math.max(perf.audience - 30, 0);
-
-        // soma um crédito extra para cada dez espectadores de comédia
-        if ("comedy" === play.type) {
+        // add extra credit for every ten comedy attendees
+        if (play.type === "comedy") {
             volumeCredits += Math.floor(perf.audience / 5);
         }
 
-        // exibe a linha
-        result += ` ${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
+        // print line for this order
+        result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
         totalAmount += thisAmount;
     }
 
-    result += `Amount owed is ${format(totalAmount/100)}\n`;
-    result += `You earnd ${volumeCredits} credits\n`;
+    result += `Amount owed is ${format(totalAmount / 100)}\n`;
+    result += `You earned ${volumeCredits} credits\n`;
 
     return result;
 }
 
 function main(){
-    console.log(Statement (invoices, plays));
+    console.log(statement (invoices, plays));
 }
 
 main();
